ce35e4e37faabc9ebc7d8de343fbbe84127881bb
Roman784 <rmironenko50@gmail.com>
2024-05-02 19:43:15 +0300

    Отделение списка модулей в отдельный компонент

--- frontend/src/pages/BoardPage.tsx
+++ frontend/src/pages/BoardPage.tsx
@@ -1,55 +1,9 @@
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-
-import UserType from "../Types/UserType";
-
-import Module from "../components/module/Module";
-import ModuleFaceType from "../Types/ModuleFaceType";
+import ModuleList from "../components/module/ModuleList";
 
 export default function BoardPage() {
-  const [modules, setModules] = useState<ModuleFaceType[]>([]);
-
-  const user = useSelector<any, UserType>(state => state.user);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (!user.isLogin) {
-      navigate("/login");
-      return;
-    }
-
-    loadModules();
-  }, []);
-
-  async function loadModules() {
-    const response = await fetch("https://localhost:7010/modules", 
-      {
-        method: "GET",
-        headers: {
-          "Accept": "application/json",
-          "Authorization": "Bearer " + user.accessToken
-        }
-    });
-    const responsedModules = await response.json()
-    
-    let temp: ModuleFaceType[] = [];
-
-    responsedModules.forEach((module: ModuleFaceType) => {
-      temp.push({
-        id: module.id,
-        title: module.title
-      });
-    });
-
-    setModules(temp);
-  }
-
   return (
     <>
-      {modules.length > 0 && modules.map((module: ModuleFaceType) => (
-        <Module key={module.id} title={module.title} />
-      ))}
+      <ModuleList requestUrl="https://localhost:7010/modules" />
     </>
   );
 }
